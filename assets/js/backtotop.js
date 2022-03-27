f4ca30bc1e9ba892ad37ea530681cd406338a122
pir8radio <pir8radio@gmail.com>
2022-03-27 11:47:53 -0500

    Add files via upload
    opacity 0.8

--- assets/js/backtotop.js
+++ assets/js/backtotop.js
@@ -32,6 +32,7 @@ function createButton(obj, pageSimulator) {
     backToTopButton.style.bottom = "0px";
     backToTopButton.style.right = "0px";
     backToTopButton.style.cursor = "pointer";
+    backToTopButton.style.opacity = "0.8";
     backToTopButton.style.textAlign = "center";
     backToTopButton.style.border = "solid 2px currentColor";
     backToTopButton.innerHTML = '<svg class="back-to-top-button-svg" xmlns="http://www.w3.org/2000/svg" width="32" height="32" viewBox="0 0 32 32" > <g fill="none" fill-rule="evenodd"> <path d="M0 0H32V32H0z" transform="translate(-1028 -172) translate(832 140) translate(32 32) translate(164) matrix(1 0 0 -1 0 32)" /> <path class="back-to-top-button-img" fill-rule="nonzero" d="M11.384 13.333h9.232c.638 0 .958.68.505 1.079l-4.613 4.07c-.28.246-.736.246-1.016 0l-4.613-4.07c-.453-.399-.133-1.079.505-1.079z" transform="translate(-1028 -172) translate(832 140) translate(32 32) translate(164) matrix(1 0 0 -1 0 32)" /> </g> </svg>';
